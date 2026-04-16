8c7ddf7a3f822f471031f7650cf4d489272d5e96
Himukai-Kanata <2184229062@qq.com>
2026-04-16 17:40:28 +0800

    fix: set anime mode to local and add localData array

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,4 @@
-	// ... 前面的配置 ...
-	siteURL: "https://himukai-kanata.github.io/Mizuki/", // GitHub Pages URL
+	anime: {
+		mode: "local", // 番剧页面模式："bangumi" 使用Bangumi API，"local" 使用本地配置，"bilibili" 使用Bilibili API
+		localData: [] // 本地番剧数据，当 mode 为 "local" 时使用
+	},
