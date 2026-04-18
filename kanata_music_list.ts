b35638f6f4fdcd6a9bc0fdc5ba1ceef71735bde6
Himukai-Kanata <2184229062@qq.com>
2026-04-18 21:04:22 +0800

    更新音乐列表配置

---
+++ kanata_music_list.ts
@@ -0,0 +1,14 @@
+const kanataMusicList = [
+	{
+		name: "日向かなたの探偵部",
+		artist: "紙の上の魔法使い",
+		url: "/assets/music/url/日向かなたの探偵部 MP3 - Kami no Ue no MahoutsukaiSuisou Ginka no Istoria.mp3",
+		cover: "/assets/music/cover/kanata.webp",
+	},
+	{
+		name: "届かない恋",
+		artist: "上原れな",
+		url: "/assets/music/url/届かない恋.mp3",
+		cover: "/assets/music/cover/todokanai.webp",
+	},
+];
