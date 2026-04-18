69a2100962359a70904ff835ecf2103d5cf08cdc
Himukai-Kanata <2184229062@qq.com>
2026-04-18 21:31:40 +0800

    简化音乐文件路径配置

--- src/config.ts
+++ src/config.ts
@@ -378,13 +378,13 @@ const kanataMusicList = [
 	{
 		name: "日向かなたの探偵部",
 		artist: "紙の上の魔法使い",
-		url: "/assets/music/url/日向かなたの探偵部 MP3 - Kami no Ue no MahoutsukaiSuisou Ginka no Istoria.mp3",
+		url: "/assets/music/url/kanata.mp3",
 		cover: "/assets/music/cover/kanata.webp",
 	},
 	{
 		name: "届かない恋",
 		artist: "上原れな",
-		url: "/assets/music/url/届かない恋.mp3",
+		url: "/assets/music/url/todokanai.mp3",
 		cover: "/assets/music/cover/todokanai.webp",
 	},
 ];
